4d1956d032a9be7177702613193025738c1cb27b
Evan Huang <evanhuang47@gmail.com>
2025-08-10 17:46:04 -0400

    chore: bumped @spounge-protos to v1.2.3 for auth RPC and messages

--- internal/secrets/secrets.go
+++ internal/secrets/secrets.go
@@ -2,7 +2,6 @@ package secrets
 
 import "context"
 
-// BootstrapSecretProvider is an interface for retrieving bootstrap secrets.
 type BootstrapSecretProvider interface {
 	GetSecret(ctx context.Context, name string) (string, error)
 }
